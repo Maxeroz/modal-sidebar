7c7a5918e2e8b7b6458674796f14a80ce74d3d66
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-04-22 23:19:11 +0400

    Sidebar Complete

--- src/context.js
+++ src/context.js
@@ -3,7 +3,38 @@ import { useState, useContext, createContext } from "react";
 const AppContext = createContext();
 
 const AppProvider = function ({ children }) {
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 const useAppContext = function () {
