cee59570bf8f776b3aa74da85f053534e2206d35
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-04-22 22:57:31 +0400

    SideBar Global Context

--- src/context.js
+++ src/context.js
@@ -1,1 +1,17 @@
-import React, { useState, useContext } from 'react'
+import { useState, useContext, createContext } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = function ({ children }) {
+  return <AppContext.Provider>{children}</AppContext.Provider>;
+};
+
+const useAppContext = function () {
+  const context = useContext(AppContext);
+
+  if (context === undefined) throw new Error("Used outside of AppProvider");
+
+  return context;
+};
+
+export { AppProvider, useAppContext };
